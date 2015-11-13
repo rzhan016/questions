a7bfd9dad025d05d11797b0ffc5a0d28250bc41c
rzhan016 <rzhan016@ucr.edu>
2015-11-13 11:42:00 -0800

    rzhan016 p2
    adding solution for p2

--- problems/p2/addition.h
+++ problems/p2/addition.h
@@ -1,17 +1,28 @@
 static int methodFor(int n)
 {
-	(void)n;
-	return 0;
+    int ans = 0;
+    for(int a=0; a<=n; ++a)
+        ans += a;
+    return ans;
 }
 
 static int methodWhile(int n)
 {
-	(void)n;
-	return 0;
+    int count = 0;
+    int ans = 0;
+    while(count<=n)
+    {
+        ans += count;
+        ++count;
+    }
 }
 
 static int methodRecurse(int n)
 {
-	(void)n;
-	return 0;
+    int ans = 0;
+    if (n == 1)
+        return (1);
+    else
+        ans = n + methodRecurse(n - 1);
+    return (ans);
 }
