85469a891b4ead98345d31cff04614f5dfc7e030
rzhan016 <rzhan016@ucr.edu>
2015-11-13 11:16:02 -0800

    rzhan016 p1
    added my solution.

--- problems/p1/fizzbuzz.h
+++ problems/p1/fizzbuzz.h
@@ -1,5 +1,17 @@
 #include <string>
+#include <sstream>
 static std::string fizzbuzz()
 {
-	return "";
+    	std::stringstream convert;
+	for(int i=0; i<=100; i++)
+	{
+		convert << i << " ";
+		if((i%3)==0 && (i%5)!=0)
+		    convert << "Fizz ";
+		if((i%3)!=0 && (i%5)==0)
+		    convert << "Buzz ";
+		if((i%3)==0 && (i%5)==0)
+		    convert << "FizzBuzz ";
+	}
+	return convert.str();
 }
